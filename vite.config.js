cebdde2eaa4c459571e6e3b1f07d5cac7d875cd1
Mark Humphreys <mark@mrlhumphreys.com>
2024-09-19 19:31:27 +1000

    silence legacy js api warnings

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["legacy-js-api"]
+      }
+    }
+  },
 	plugins: [sveltekit()],
   server: {
     host: true,
