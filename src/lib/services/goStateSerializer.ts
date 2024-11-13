24d48d52c07b1d93da5684b17a4fd6900cb2bed9
Mark Humphreys <mark@mrlhumphreys.com>
2024-11-13 20:14:40 +1100

    go - fix serialization bug around stone order

--- src/lib/services/goStateSerializer.ts
+++ src/lib/services/goStateSerializer.ts
@@ -1,4 +1,5 @@
 import type GameState from '$lib/go/interfaces/GameState';
+import type Point from '$lib/go/interfaces/Point';
 
 const ID_TO_POS = [
   '--',
@@ -392,8 +393,16 @@ const ID_TO_POS = [
 const goStateSerializer = function(state: GameState): string {
   let currentPlayerCode = (state.currentPlayerNumber === 2 ? 'W' : 'B');
 
-  let blackStones = state.points.filter(function(p) { return p.stone !== null && p.stone.playerNumber === 1; }).map(function(p) { return `[${ID_TO_POS[p.id]}]`; }).join('');
-  let whiteStones = state.points.filter(function(p) { return p.stone !== null && p.stone.playerNumber === 2; }).map(function(p) { return `[${ID_TO_POS[p.id]}]`; }).join('');
+  let stoneComparisonFunction = function(a: Point, b: Point): number {
+    if (a.stone !== null && b.stone !== null) {
+      return a.stone.id - b.stone.id;
+    } else {
+      return 0;
+    }
+  };
+
+  let blackStones = state.points.filter(function(p) { return p.stone !== null && p.stone.playerNumber === 1; }).sort(stoneComparisonFunction).map(function(p) { return `[${ID_TO_POS[p.id]}]`; }).join('');
+  let whiteStones = state.points.filter(function(p) { return p.stone !== null && p.stone.playerNumber === 2; }).sort(stoneComparisonFunction).map(function(p) { return `[${ID_TO_POS[p.id]}]`; }).join('');
 
   let blackPlayerStat = state.playerStats.find(function(ps) { return ps.playerNumber === 1; });
   let blackPrisonerCount = (blackPlayerStat !== undefined ? blackPlayerStat.prisonerCount : 0);
--- src/test/services/goStateSerializer.test.ts
+++ src/test/services/goStateSerializer.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import goStateSerializer from '$lib/services/goStateSerializer';
 import buildMatchAttributes from '$lib/go/logic/buildMatchAttributes';
+import koRuleMatchTwo from '../go/fixtures/koRuleMatchTwo';
 
 describe('goStateSerializer', () => {
   it('serializes the state', () => {
@@ -12,11 +13,10 @@ describe('goStateSerializer', () => {
   });
 
   it('serializes state with pervious captures', () => {
-    let match = buildMatchAttributes(1);
+    let match = koRuleMatchTwo();
     let gameState = match.gameState;
-    gameState.previousState = "-2-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
     let result = goStateSerializer(gameState);
-    let expected = 'PL[B]ABAWXB[0]XW[0]XS[ba]';
+    let expected = 'PL[W]AB[pd][cb][eb][dc][fa][da][ib][kb][jc][ja]AW[aa][ba][ca][ga][ha][ia][ka][la]XB[3]XW[2]XS[jb]';
     expect(result).toEqual(expected);
   });
 });
